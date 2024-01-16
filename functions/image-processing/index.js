736fc30b9c2ad9047f6d3a6cbbf08c0c9f61d677
jamesnguyentekcent <86237387+jamesnguyentekcent@users.noreply.github.com>
2024-01-16 15:27:04 +0700

    Update index.js

--- functions/image-processing/index.js
+++ functions/image-processing/index.js
@@ -9,23 +9,53 @@ const S3 = new AWS.S3({ signatureVersion: 'v4', httpOptions: { agent: new https.
 const S3_ORIGINAL_IMAGE_BUCKET = process.env.originalImageBucketName;
 const S3_TRANSFORMED_IMAGE_BUCKET = process.env.transformedImageBucketName;
 const TRANSFORMED_IMAGE_CACHE_TTL = process.env.transformedImageCacheTTL;
+
+// For image auto resizing only
+const PREDEFINED_TRANSFORMED_IMAGE_SIZES = process.env.predefinedTransformedImageSizes;
+const PREDEFINED_TRANSFORMED_IMAGE_FORMATS = process.env.predefinedTransformedImageFormats;
+
 const SECRET_KEY = process.env.secretKey;
 const MAX_IMAGE_SIZE = parseInt(process.env.maxImageSize);
 
 exports.handler = async (event) => {
-    // First validate if the request is coming from CloudFront
-    if (!event.headers['x-origin-secret-header'] || !(event.headers['x-origin-secret-header'] === SECRET_KEY)) return sendError(403, 'Request unauthorized', event);
-    // Validate if this is a GET request
-    if (!event.requestContext || !event.requestContext.http || !(event.requestContext.http.method === 'GET')) return sendError(400, 'Only GET method is supported', event);
-    // An example of expected path is /images/rio/1.jpeg/format=auto,width=100 or /images/rio/1.jpeg/original where /images/rio/1.jpeg is the path of the original image
-    var imagePathArray = event.requestContext.http.path.split('/');
-    // get the requested image operations
-    var operationsPrefix = imagePathArray.pop();
-    // get the original image path images/rio/1.jpg
-    imagePathArray.shift();
-    var originalImagePath = imagePathArray.join('/');
+	let originalImagePath;
+	let operationsPrefix;
+	// Get the object from the event and show its content type
+    if(event.Records != null && event.Records[0].eventName.includes('ObjectCreated'))
+    {
+		originalImagePath = event.Records[0].s3.object.key;
+		// Expected sample value: 'width=360&height=270;height=480;width=1280'
+		const predefinedTransformedImageSizes = PREDEFINED_TRANSFORMED_IMAGE_SIZES.split(';');
+		// Expected sample value: 'jpeg;avif;webp'
+		const predefinedTransformedImageFormats = PREDEFINED_TRANSFORMED_IMAGE_FORMATS.split(';');
 
-    var startTime = performance.now();
+		// Handling image transformations
+		predefinedTransformedImageSizes.forEach(async (size) => {
+			predefinedTransformedImageFormats.forEach(async (format) => {
+				const operationsPrefix = `${size},format=${format}`;
+				await transformImage(originalImagePath, operationsPrefix); });});
+    } else {
+		// First validate if the request is coming from CloudFront
+		if (!event.headers['x-origin-secret-header'] || !(event.headers['x-origin-secret-header'] === SECRET_KEY)) return sendError(403, 'Request unauthorized', event);
+		// Validate if this is a GET request
+		// An example of expected path is /sample/1.jpeg/format=auto,width=100 or /sample/1.jpeg/original where /sample/1.jpeg is the path of the original image
+		if (!event.requestContext || !event.requestContext.http || !(event.requestContext.http.method === 'GET')) return sendError(400, 'Only GET method is supported', event);
+		var imagePathArray = event.requestContext.http.path.split('/');
+		// get the requested image operations
+		operationsPrefix = imagePathArray.pop();
+		// get the original image path sample/1.jpg
+		imagePathArray.shift();
+		originalImagePath = imagePathArray.join('/');
+		// Handling image transformations
+		await transformImage(originalImagePath, operationsPrefix);
+	}
+
+};
+
+// Handling image transformations
+async function transformImage(originalImagePath, operationsPrefix) {
+	var startTime = performance.now();
+	
     // Downloading original image
     let originalImage;
     let contentType;
@@ -35,12 +65,14 @@ exports.handler = async (event) => {
     } catch (error) {
         return sendError(500, 'error downloading original image', error);
     }
+	
+	var operationsPrefixArray = event.requestContext.http.path.split('/');
     let transformedImage = Sharp(originalImage.Body, { failOn: 'none', animated: true });
     // Get image orientation to rotate if needed
     const imageMetadata = await transformedImage.metadata();
-    //  execute the requested operations 
+    // Execute the requested operations 
     const operationsJSON = Object.fromEntries(operationsPrefix.split(',').map(operation => operation.split('=')));
-    // variable holding the server timing header value
+    // Variable holding the server timing header value
     var timingLog =  'img-download;dur=' + parseInt(performance.now() - startTime);
     startTime = performance.now();
     try {
@@ -72,8 +104,8 @@ exports.handler = async (event) => {
     } catch (error) {
         return sendError(500, 'error transforming image', error);
     }
-    timingLog = timingLog + ',img-transform;dur=' +parseInt(performance.now() - startTime);
-
+    timingLog = timingLog + ',img-transform;dur=' + parseInt(performance.now() - startTime);
+    
     // Graceful handleing of generated images bigger than a specified limit (e.g. Lambda output object limit)
     const imageTooBig = Buffer.byteLength(transformedImage) > MAX_IMAGE_SIZE;
 
@@ -121,7 +153,7 @@ exports.handler = async (event) => {
             'Server-Timing': timingLog
         }
     };
-};
+}
 
 function sendError(statusCode, body, error) {
     logError(body, error);
